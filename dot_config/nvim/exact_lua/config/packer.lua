b578db0bcbd0797c986bed42b37d9ed4dfc6ab47
Christian Rackerseder <git@echooff.de>
2023-03-02 21:28:19 +0100

    Fix filename in Packer configuration

--- dot_config/nvim/exact_lua/config/packer.lua
+++ dot_config/nvim/exact_lua/config/packer.lua
@@ -14,7 +14,7 @@ local packer_bootstrap = ensure_packer()
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost packer.lua source <afile> | PackerCompile
   augroup end
 ]])
 
