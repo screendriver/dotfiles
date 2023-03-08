d19c483ac557fb9ec6501250a733cb0ca9a28832
Christian Rackerseder <git@echooff.de>
2023-03-08 22:42:24 +0100

    Add missing Lua function

--- dot_config/nvim/exact_lua/plugins/conquer-of-completion.lua
+++ dot_config/nvim/exact_lua/plugins/conquer-of-completion.lua
@@ -1,3 +1,8 @@
+function _G.check_back_space()
+	local col = vim.fn.col('.') - 1
+	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
 return {
 	"neoclide/coc.nvim",
 	branch = "release",
