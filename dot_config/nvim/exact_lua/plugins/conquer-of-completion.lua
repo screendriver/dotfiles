f88f2916c6adf4dbe3d57bf0c6cc1544c68416c6
Christian Rackerseder <git@echooff.de>
2023-03-09 09:26:31 +0100

    Add missing Lua function to show documentations

--- dot_config/nvim/exact_lua/plugins/conquer-of-completion.lua
+++ dot_config/nvim/exact_lua/plugins/conquer-of-completion.lua
@@ -3,6 +3,17 @@ function _G.check_back_space()
 	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
+function _G.show_docs()
+	local cw = vim.fn.expand('<cword>')
+	if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command('h ' .. cw)
+	elseif vim.api.nvim_eval('coc#rpc#ready()') then
+		vim.fn.CocActionAsync('doHover')
+	else
+		vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+	end
+end
+
 return {
 	"neoclide/coc.nvim",
 	branch = "release",
