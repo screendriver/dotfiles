6a4305f0bccd4a95c3763f859c1e54817dca56b4
Christian Rackerseder <git@echooff.de>
2023-03-11 08:03:31 +0100

    Configure indent-blankline

--- dot_config/nvim/exact_lua/plugins/indent-blankline.lua
+++ dot_config/nvim/exact_lua/plugins/indent-blankline.lua
@@ -4,5 +4,9 @@ return {
 	opts = {
 		show_end_of_line = false,
 		space_char_blankline = " ",
+		use_treesitter = true,
+		show_first_indent_level = false,
+		show_trailing_blankline_indent = false,
+		show_current_context = true
 	}
 }
