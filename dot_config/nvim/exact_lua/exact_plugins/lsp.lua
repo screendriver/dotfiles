b1663b42650c2a5932f2cc287336398679ffc407
Christian Rackerseder <git@echooff.de>
2023-03-13 12:38:31 +0100

    Add nvim-autopairs to nvim-cmp

--- dot_config/nvim/exact_lua/exact_plugins/lsp.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lsp.lua
@@ -75,6 +75,9 @@ return {
 		config = function()
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 			cmp.setup({
 				snippet = {
