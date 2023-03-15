bab722b49ac4228d7952fe906c133536706515d4
Christian Rackerseder <git@echooff.de>
2023-03-15 18:45:53 +0100

    Remove lazy.nvim package status from lualine

--- dot_config/nvim/exact_lua/exact_plugins/lualine.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lualine.lua
@@ -16,14 +16,7 @@ return {
 					"filename",
 					path = 1,
 				} },
-				lualine_x = {
-					{
-						require("lazy.status").updates,
-						cond = require("lazy.status").has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					"filetype",
-				},
+				lualine_x = { "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
