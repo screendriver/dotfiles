176132c6ed602173b4ac0961ac2f8ea3251af6a0
Christian Rackerseder <git@echooff.de>
2023-03-17 09:39:33 +0100

    Highlight symbol under cursor

--- dot_config/nvim/exact_lua/exact_plugins/lsp.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lsp.lua
@@ -31,8 +31,33 @@ return {
 			local on_attach = function(client, buffer)
 				-- Format current buffer
 				vim.api.nvim_buf_create_user_command(buffer, "Format", function()
-					vim.lsp.buf.format({ async = true })
+					vim.lsp.buf.format({ async = true, bufnr = vim.api.nvim_get_current_buf() })
 				end, { desc = "Format current buffer" })
+				-- Highlight symbol under cursor
+				if client.server_capabilities.documentHighlightProvider then
+					vim.cmd([[
+    hi! LspReferenceRead cterm=bold ctermbg=red guibg=#6272a4
+    hi! LspReferenceText cterm=bold ctermbg=red guibg=#6272a4
+    hi! LspReferenceWrite cterm=bold ctermbg=red guibg=#6272a4
+  ]])
+					vim.api.nvim_create_augroup("lsp_document_highlight", {
+						clear = false,
+					})
+					vim.api.nvim_clear_autocmds({
+						buffer = bufnr,
+						group = "lsp_document_highlight",
+					})
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						group = "lsp_document_highlight",
+						buffer = bufnr,
+						callback = vim.lsp.buf.document_highlight,
+					})
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						group = "lsp_document_highlight",
+						buffer = bufnr,
+						callback = vim.lsp.buf.clear_references,
+					})
+				end
 			end
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
