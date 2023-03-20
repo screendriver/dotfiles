db89e5df910e199c0cbe64580967b6de24b9dc01
Christian Rackerseder <git@echooff.de>
2023-03-20 15:19:40 +0100

    Add keymap for toggling outline

--- dot_config/nvim/exact_lua/exact_plugins/lsp.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lsp.lua
@@ -201,6 +201,7 @@ return {
 			{ "gt", "<cmd>Lspsaga goto_type_definition<CR>", desc = "Go to [T]ype definiton" },
 			{ "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "Show [L]ine [D]iagnostics" },
 			{ "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", desc = "Show [C]cursor [D]iagnostics" },
+			{ "<leader>o", "<cmd>Lspsaga outline<CR>", desc = "Toggle [O]utline" },
 			{ "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Go to next [D]iagnostics" },
 			{ "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Go to previous [D]iagnostics" },
 			{ "K", "<cmd>Lspsaga hover_doc<CR>", desc = "Show documentation in preview window" },
