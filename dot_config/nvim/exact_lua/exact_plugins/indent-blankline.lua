931af463e81c5b86486401652a12a73a2f97eed9
Christian Rackerseder <github@echooff.de>
2023-10-11 13:32:42 +0200

    Update indent-blankline neovim plugin configuration
    Because of the new major v3 update

--- dot_config/nvim/exact_lua/exact_plugins/indent-blankline.lua
+++ dot_config/nvim/exact_lua/exact_plugins/indent-blankline.lua
@@ -1,13 +1,16 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	event = { "BufReadPre", "BufNewFile" },
+	event = { "BufReadPost", "BufNewFile" },
 	opts = {
-		show_end_of_line = false,
-		space_char_blankline = " ",
-		use_treesitter = true,
-		show_first_indent_level = false,
-		show_trailing_blankline_indent = false,
-		show_current_context = true,
+		scope = {
+			show_start = false,
+		},
 	},
+	config = function(_, opts)
+		local hooks = require("ibl.hooks")
+		hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+
+		require("ibl").setup(opts)
+	end,
 }
