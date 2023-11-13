ef9713c2f1715ca95eb5fe83e091567319d397db
Christian Rackerseder <github@echooff.de>
2023-11-13 17:12:49 +0100

    Add oil.nvim file explorer

---
+++ dot_config/nvim/exact_lua/exact_plugins/file-explorer.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	opts = {
+		default_file_explorer = true,
+		columns = {
+			"icon",
+			"size",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>oi",
+			"<cmd>Oil --float<CR>",
+			silent = true,
+			noremap = true,
+			desc = "Open [Oi]l in a floating window",
+		},
+	},
+}
