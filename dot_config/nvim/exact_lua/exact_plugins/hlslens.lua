d52a685e2a3b9db6a3c4a4d2c941695fbe3dec22
Christian Rackerseder <github@echooff.de>
2023-12-06 10:14:41 +0100

    Remove keymap to hide search results

--- dot_config/nvim/exact_lua/exact_plugins/hlslens.lua
+++ dot_config/nvim/exact_lua/exact_plugins/hlslens.lua
@@ -21,7 +21,5 @@ return {
 		vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 		vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
 		vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-
-		vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
 	end,
 }
