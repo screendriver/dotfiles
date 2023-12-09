f1c0e4425827380b1ddd737bd572c5167d263180
Christian Rackerseder <github@echooff.de>
2023-12-09 07:50:23 +0100

    Create new user commands for formatting a buffer in neovim

--- dot_config/nvim/exact_lua/exact_plugins/formatter.lua
+++ dot_config/nvim/exact_lua/exact_plugins/formatter.lua
@@ -1,6 +1,5 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
 		formatters_by_ft = {
@@ -17,11 +16,44 @@ return {
 		},
 		format_on_save = function(bufnr)
 			local bufname = vim.api.nvim_buf_get_name(bufnr)
-			if bufname:match("/node_modules/") then
+			if bufname:match("/node_modules/") or vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
 
 			return { timeout_ms = 500, lsp_fallback = true }
 		end,
 	},
+	init = function()
+		local conform = require("conform")
+
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			conform.format({ async = true, lsp_fallback = true, range = range })
+		end, { desc = "Format buffer", range = true })
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
 }
