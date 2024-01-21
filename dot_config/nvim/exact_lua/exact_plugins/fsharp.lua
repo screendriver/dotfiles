464734d31c3600217e617cc22933b100bfaf3cf3
Christian Rackerseder <github@echooff.de>
2024-01-21 16:33:11 +0100

    Remove F# neovim plugin
    https://github.com/ionide/Ionide-vim

--- dot_config/nvim/exact_lua/exact_plugins/fsharp.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"ionide/Ionide-vim",
-		init = function()
-			vim.g["fsharp#backend"] = "nvim"
-			vim.g["fsharp#fsautocomplete_command"] = vim.api.nvim_eval('["fsautocomplete"]')
-		end,
-	},
-}
