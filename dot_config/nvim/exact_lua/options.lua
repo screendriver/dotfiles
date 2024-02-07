b72d8c4d75e8760964d6f353514a15e5e6e8d7b5
Christian Rackerseder <github@echooff.de>
2024-02-07 13:25:24 +0100

    Use local variables in Neovim options

--- dot_config/nvim/exact_lua/options.lua
+++ dot_config/nvim/exact_lua/options.lua
@@ -1,41 +1,44 @@
-vim.g.mapleader = " " -- Use <Space> as leader key
-vim.opt.number = true -- Show line numbers
-vim.opt.relativenumber = true -- Show line number relative to the current line
-vim.opt.swapfile = false -- Disable swap file
+local g = vim.g
+local opt = vim.opt
+
+g.mapleader = " " -- Use <Space> as leader key
+opt.number = true -- Show line numbers
+opt.relativenumber = true -- Show line number relative to the current line
+opt.swapfile = false -- Disable swap file
 
 -- Search
-vim.opt.ignorecase = true -- Ignore letter case when searching
-vim.opt.smartcase = true -- Case insentive unless capitals used in search
-vim.opt.hlsearch = true -- Highlight search results
-vim.opt.incsearch = true -- Set incremental search, like modern Browsers
+opt.ignorecase = true -- Ignore letter case when searching
+opt.smartcase = true -- Case insentive unless capitals used in search
+opt.hlsearch = true -- Highlight search results
+opt.incsearch = true -- Set incremental search, like modern Browsers
 
-vim.opt.cursorline = true -- Find the current line quickly
-vim.opt.mouse = "a" -- Enable mouse support
+opt.cursorline = true -- Find the current line quickly
+opt.mouse = "a" -- Enable mouse support
 
 -- Indentation
-vim.opt.tabstop = 4 -- 1 Tab = 4 Spaces
-vim.opt.shiftwidth = 4 -- shift 4 spaces
-vim.opt.autoindent = true -- Copy indent from current line when starting a new line
-vim.opt.smartindent = false -- Don't smart autoindenting when starting a new line. Treesitter is doing that for us
-vim.opt.expandtab = false -- Don't insert <Tab> when too many spaces are inserted
+opt.tabstop = 4 -- 1 Tab = 4 Spaces
+opt.shiftwidth = 4 -- shift 4 spaces
+opt.autoindent = true -- Copy indent from current line when starting a new line
+opt.smartindent = false -- Don't smart autoindenting when starting a new line. Treesitter is doing that for us
+opt.expandtab = false -- Don't insert <Tab> when too many spaces are inserted
 
-vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
+opt.list = true
+opt.listchars:append("space:⋅")
 
 -- Split natural directions
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Error bells
-vim.opt.errorbells = false
-vim.opt.visualbell = true
+opt.errorbells = false
+opt.visualbell = true
 
-vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved
-vim.opt.updatetime = 350 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable delays and poor user experience
-vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" } -- Trigger a quick-scope highlight
-vim.g.loaded_netrw = true
-vim.g.loaded_netrwPlugin = true
-vim.opt.termguicolors = true -- Force GUI colors in terminals
-vim.opt.iskeyword:append("-") -- Words-with-a-dash should be treated as a whole word
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved
+opt.updatetime = 350 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable delays and poor user experience
+g.qs_highlight_on_keys = { "f", "F", "t", "T" } -- Trigger a quick-scope highlight
+g.loaded_netrw = true
+g.loaded_netrwPlugin = true
+opt.termguicolors = true -- Force GUI colors in terminals
+opt.iskeyword:append("-") -- Words-with-a-dash should be treated as a whole word
 
-vim.opt.conceallevel = 1 -- Display JSON files without any quotes
+opt.conceallevel = 1 -- Display JSON files without any quotes
