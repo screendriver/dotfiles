d375c4efe677d773e9bae21a0cd97de786e32760
Christian Rackerseder <git@echooff.de>
2024-06-25 15:42:55 +0200

    Update lazy.nvim to v11

---
+++ dot_config/nvim/exact_lua/config/lazy.lua
@@ -0,0 +1,35 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		{
+			"nvim-lua/plenary.nvim",
+			lazy = true,
+		},
+		{
+			"nvim-tree/nvim-web-devicons",
+			lazy = true,
+			config = true,
+		},
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- do not automatically check for plugin updates
+	checker = { enabled = false },
+})
