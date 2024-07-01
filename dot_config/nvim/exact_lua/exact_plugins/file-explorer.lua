bd0d62247312ad36bb6eafbddd5f8183b03f605f
Christian Rackerseder <git@echooff.de>
2024-07-01 11:11:23 +0200

    Remove oil.nvim and exclusively use yazi.nvim

--- dot_config/nvim/exact_lua/exact_plugins/file-explorer.lua
+++ dot_config/nvim/exact_lua/exact_plugins/file-explorer.lua
@@ -1,57 +1,28 @@
+---@type LazySpec
 return {
-	{
-		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		event = "VeryLazy",
-		opts = {
-			default_file_explorer = true,
-			columns = {
-				"icon",
-			},
-			view_options = {
-				show_hidden = true,
-			},
-			float = {
-				win_options = {
-					winblend = 3,
-				},
-			},
+	"mikavilpas/yazi.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>yy",
+			function()
+				require("yazi").yazi()
+			end,
+			desc = "Open Yazi file manager",
 		},
-		keys = {
-			{
-				"-",
-				"<CMD>Oil<CR>",
-				noremap = true,
-				desc = "Open parent directory",
-			},
+		{
+			"<leader>cw",
+			function()
+				require("yazi").yazi(nil, vim.fn.getcwd())
+			end,
+			desc = "Open Yazi file manager in nvim's working directory",
 		},
 	},
-	---@type LazySpec
-	{
-		"mikavilpas/yazi.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		event = "VeryLazy",
-		keys = {
-			{
-				"<leader>yy",
-				function()
-					require("yazi").yazi()
-				end,
-				desc = "Open Yazi file manager",
-			},
-			{
-				"<leader>cw",
-				function()
-					require("yazi").yazi(nil, vim.fn.getcwd())
-				end,
-				desc = "Open Yazi file manager in nvim's working directory",
-			},
-		},
-		---@type YaziConfig
-		opts = {
-			open_for_directories = false,
-		},
+	---@type YaziConfig
+	opts = {
+		open_for_directories = false,
 	},
 }
