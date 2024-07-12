e55b0db45f623f0741bc235d44e4c1de5d810eb8
Christian Rackerseder <git@echooff.de>
2024-07-12 15:02:10 +0200

    Show jump labels when jumping to character

--- dot_config/nvim/exact_lua/exact_plugins/flash.lua
+++ dot_config/nvim/exact_lua/exact_plugins/flash.lua
@@ -2,7 +2,13 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		modes = {
+			char = {
+				jump_labels = true,
+			},
+		},
+	},
 	-- stylua: ignore
 	keys = {
 		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
