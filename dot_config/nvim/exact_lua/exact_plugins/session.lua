16f46adb65e81e93ced8796bb98241289d31042a
Christian Rackerseder <git@echooff.de>
2025-04-02 09:42:21 +0200

    Replace telescope with snacks.nvim picker

--- dot_config/nvim/exact_lua/exact_plugins/session.lua
+++ dot_config/nvim/exact_lua/exact_plugins/session.lua
@@ -1,16 +1,9 @@
 return {
 	"olimorris/persisted.nvim",
-	dependencies = {
-		"nvim-telescope/telescope.nvim",
-	},
 	lazy = false,
 	opts = {
 		use_git_branch = false,
 		autosave = true,
 		autoload = true,
 	},
-	config = function(pluginName, opts)
-		require("persisted").setup(opts)
-		require("telescope").load_extension("persisted")
-	end,
 }
--- dot_config/nvim/exact_lua/exact_plugins/snacks.lua
+++ dot_config/nvim/exact_lua/exact_plugins/snacks.lua
@@ -4,6 +4,9 @@ return {
 	lazy = false,
 	opts = {
 		lazygit = {},
+		picker = {
+			enabled = true,
+		},
 	},
 	keys = {
 		{
@@ -11,7 +14,7 @@ return {
 			function()
 				Snacks.lazygit()
 			end,
-			desc = "Open [L]azy[G]it",
+			desc = "Open Lazygit",
 		},
 		{
 			"<leader>bd",
@@ -19,6 +22,148 @@ return {
 				Snacks.bufdelete()
 			end,
 			desc = "Delete Buffer",
+		},
+		{
+			"<leader>fs",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart find files",
+		},
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find files",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Find buffers",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Find with grep",
+		},
+		{
+			"<leader>sw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Visual selection or word",
+			mode = { "n", "x" },
+		},
+		{
+			'<leader>s"',
+			function()
+				Snacks.picker.registers()
+			end,
+			desc = "Search in registers",
+		},
+		{
+			"<leader>sb",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "Search buffer lines",
+		},
+		{
+			"<leader>sd",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+			desc = "Search diagnostics",
+		},
+		{
+			"<leader>sD",
+			function()
+				Snacks.picker.diagnostics_buffer()
+			end,
+			desc = "Search buffer diagnostics",
+		},
+		{
+			"<leader>sh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "Search in help pages",
+		},
+		{
+			"<leader>sj",
+			function()
+				Snacks.picker.jumps()
+			end,
+			desc = "Search in jumps",
+		},
+		{
+			"<leader>sk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+			desc = "Search keymaps",
+		},
+		{
+			"<leader>sm",
+			function()
+				Snacks.picker.marks()
+			end,
+			desc = "Search marks",
+		},
+		{
+			"<leader>su",
+			function()
+				Snacks.picker.undo()
+			end,
+			desc = "Search in undo history",
+		},
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "Goto Definition",
+		},
+		{
+			"gD",
+			function()
+				Snacks.picker.lsp_declarations()
+			end,
+			desc = "Goto Declaration",
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+			nowait = true,
+			desc = "References",
+		},
+		{
+			"gI",
+			function()
+				Snacks.picker.lsp_implementations()
+			end,
+			desc = "Goto Implementation",
+		},
+		{
+			"gy",
+			function()
+				Snacks.picker.lsp_type_definitions()
+			end,
+			desc = "Goto T[y]pe Definition",
+		},
+		{
+			"<leader>ss",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+			desc = "LSP Symbols",
 		},
 	},
 }
