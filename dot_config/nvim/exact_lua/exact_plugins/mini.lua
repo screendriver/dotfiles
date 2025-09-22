64ff304edf894c12d47238cb7b0c2d8133984f9b
Christian Rackerseder <git@echooff.de>
2025-09-22 11:29:51 +0200

    Replace colorizer with mini.hipatterns

--- dot_config/nvim/exact_lua/exact_plugins/mini.lua
+++ dot_config/nvim/exact_lua/exact_plugins/mini.lua
@@ -4,6 +4,17 @@ return {
 	config = function()
 		require("mini.comment").setup()
 		require("mini.diff").setup()
+
+		local hipatterns = require("mini.hipatterns")
+		hipatterns.setup({
+			highlighters = {
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
+
 		require("mini.pairs").setup()
 	end,
 }
